b070cdcf1d4a7f9390ab8481028da7e0bf829197
tlong4856@gmail.com <71266378+shynrii@users.noreply.github.com>
2021-06-05 16:55:20 +0700

    Format code

--- app/src/main/java/com/example/appbookroom/LoginActivity.java
+++ app/src/main/java/com/example/appbookroom/LoginActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.appbookroom.databinding.ActivityLoginBinding;
@@ -21,26 +22,27 @@ import com.example.appbookroom.databinding.ActivityLoginBinding;
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
     Button btn_login;
+    EditText etEmail, etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
         btn_login = findViewById(R.id.btn_login);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Dialog dialog = new Dialog(LoginActivity.this);
-
                 WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
                 layoutParams.copyFrom(dialog.getWindow().getAttributes());
-
                 layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
                 layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
                 dialog.setContentView(R.layout.dialog_login);
                 final Button btnCancel = dialog.findViewById(R.id.btnCancel);
                 final ImageButton imgCancel = dialog.findViewById(R.id.imgCancel);
-
                 btnCancel.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -53,14 +55,12 @@ public class LoginActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
-                int width = (int) (getResources().getDisplayMetrics().widthPixels*0.92);
-                int height = (int) (getResources().getDisplayMetrics().heightPixels*0.35);
+                int width = (int) (getResources().getDisplayMetrics().widthPixels * 0.92);
+                int height = (int) (getResources().getDisplayMetrics().heightPixels * 0.35);
                 dialog.getWindow().setBackgroundDrawableResource(R.drawable.shape_dialog);
                 dialog.getWindow().setLayout(width, height);
                 dialog.show();
             }
         });
     }
-
-
 }
