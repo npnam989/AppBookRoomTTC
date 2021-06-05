5bdc9aca8e340b47d8322f06b593d858a2a7791a
tlong4856@gmail.com <71266378+shynrii@users.noreply.github.com>
2021-06-05 21:37:27 +0700

    Update Dialog

--- app/src/main/java/com/example/appbookroom/LoginActivity.java
+++ app/src/main/java/com/example/appbookroom/LoginActivity.java
@@ -2,14 +2,17 @@ package com.example.appbookroom;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBinderMapper;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,49 +21,48 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.appbookroom.databinding.ActivityLoginBinding;
+import com.example.appbookroom.databinding.DialogLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
-    Button btn_login;
-    EditText etEmail, etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
-        btn_login = findViewById(R.id.btn_login);
+        binding = ActivityLoginBinding.inflate( getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Dialog dialog = new Dialog(LoginActivity.this);
-                WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
-                layoutParams.copyFrom(dialog.getWindow().getAttributes());
-                layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
-                layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
-                dialog.setContentView(R.layout.dialog_login);
-                final Button btnCancel = dialog.findViewById(R.id.btnCancel);
-                final ImageButton imgCancel = dialog.findViewById(R.id.imgCancel);
-                btnCancel.setOnClickListener(new View.OnClickListener() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                LayoutInflater layoutInflater = LayoutInflater.from(LoginActivity.this);
+                DialogLoginBinding dialogLoginBinding = DialogLoginBinding.inflate(layoutInflater);
+                builder.setView(dialogLoginBinding.getRoot());
+                AlertDialog alertDialog = builder.create();
+                dialogLoginBinding.btnCancel.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        dialog.dismiss();
+                        alertDialog.dismiss();
                     }
                 });
-                imgCancel.setOnClickListener(new View.OnClickListener() {
+                dialogLoginBinding.imgCancel.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        dialog.dismiss();
+                        alertDialog.dismiss();
                     }
                 });
                 int width = (int) (getResources().getDisplayMetrics().widthPixels * 0.92);
                 int height = (int) (getResources().getDisplayMetrics().heightPixels * 0.35);
-                dialog.getWindow().setBackgroundDrawableResource(R.drawable.shape_dialog);
-                dialog.getWindow().setLayout(width, height);
-                dialog.show();
+
+                alertDialog.getWindow().setBackgroundDrawableResource(R.drawable.shape_dialog);
+                alertDialog.getWindow().setLayout(width, height);
+                alertDialog.show();
+
+
             }
         });
     }
+
 }
